5bba0f1171c8e89b81a8bf8d19a3e560587eb264
GoldenDude <edanazran@gmail.com>
2018-06-02 21:09:13 +0300

    Page Basics Layout

--- includes/javascript.js
+++ includes/javascript.js
@@ -1,62 +1,84 @@
-$(document).ready(function () {
-    var json_data = []
-    $.getJSON("data/MOCK_DATA.json", function (data) {
-    console.log(data);
-    json_data = data
-    for (var row of data) {
-    var table_row = $(
-    '<tr>' +
-    '<td>' + row.id + '</td>' +
-    '<td>' + row.date + '</td>' +
-    '<td>' + row.country + '</td>' +
-    '<td>' + row.city + '</td>' +
-    '<td>' + row.temp_c + '</td>' +
-    '<td>' + row.temp_f + '</td>' +
-    '</tr>'
-    )
-    var option = $(
-    '<option>' + row.country + '</option>'
-    )
-    $('#country').append(option)
-    $('tbody').append(table_row)
-    }
-    });
+ window.onload = function startTime() {    
+        var today = new Date();
+        var currentDate  = new Date(),
+            currentDay = today.getDate() < 10 ? '0' + today.getDate(): today.getDate(),
+            currentMonth = today.getMonth() < 9 ? '0' + (today.getMonth() + 1): (today.getMonth() + 1);
 
-    $('#country').on('change', function (e) {
-    var value = e.target.value;
-    var match = '';
-    console.log(value);
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById("date").innerHTML = currentDay + '/' + currentMonth + '/' +  today.getFullYear();
+        document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+}
     
-    if (value == "All"){
-        for (var row of data) {
-            var table_row = $(
-            '<tr>' +
-            '<td>' + row.id + '</td>' +
-            '<td>' + row.date + '</td>' +
-            '<td>' + row.country + '</td>' +
-            '<td>' + row.city + '</td>' +
-            '<td>' + row.temp_c + '</td>' +
-            '<td>' + row.temp_f + '</td>' +
-            '</tr>'
-            )
-            $('tbody').append(table_row);
-        }
-        return;
-    }   
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
 
 
-    for (var row of json_data) {
-    if (row.country == value)
-    match +=
-    '<tr>' +
-    '<td scope="row">' + row.id + '</td>' +
-    '<td>' + row.date + '</td>' +
-    '<td>' + row.country + '</td>' +
-    '<td>' + row.city + '</td>' +
-    '<td>' + row.temp_c + '</td>' +
-    '<td>' + row.temp_f + '</td>' +
-    '</tr>'
-    }
-    $('tbody').html(match)
-    })
-   });
+// $(document).ready(function () {
+//     var json_data = []
+//     $.getJSON("data/MOCK_DATA.json", function (data) {
+//     console.log(data);
+//     json_data = data
+//     for (var row of data) {
+//     var table_row = $(
+//     '<tr>' +
+//     '<td>' + row.id + '</td>' +
+//     '<td>' + row.date + '</td>' +
+//     '<td>' + row.country + '</td>' +
+//     '<td>' + row.city + '</td>' +
+//     '<td>' + row.temp_c + '</td>' +
+//     '<td>' + row.temp_f + '</td>' +
+//     '</tr>'
+//     )
+//     var option = $(
+//     '<option>' + row.country + '</option>'
+//     )
+//     $('#country').append(option)
+//     $('tbody').append(table_row)
+//     }
+//     });
+
+//     $('#country').on('change', function (e) {
+//     var value = e.target.value;
+//     var match = '';
+//     console.log(value);
+    
+//     if (value == "All"){
+//         for (var row of data) {
+//             var table_row = $(
+//             '<tr>' +
+//             '<td>' + row.id + '</td>' +
+//             '<td>' + row.date + '</td>' +
+//             '<td>' + row.country + '</td>' +
+//             '<td>' + row.city + '</td>' +
+//             '<td>' + row.temp_c + '</td>' +
+//             '<td>' + row.temp_f + '</td>' +
+//             '</tr>'
+//             )
+//             $('tbody').append(table_row);
+//         }
+//         return;
+//     }   
+
+
+//     for (var row of json_data) {
+//     if (row.country == value)
+//     match +=
+//     '<tr>' +
+//     '<td scope="row">' + row.id + '</td>' +
+//     '<td>' + row.date + '</td>' +
+//     '<td>' + row.country + '</td>' +
+//     '<td>' + row.city + '</td>' +
+//     '<td>' + row.temp_c + '</td>' +
+//     '<td>' + row.temp_f + '</td>' +
+//     '</tr>'
+//     }
+//     $('tbody').html(match)
+//     })
+//    });
