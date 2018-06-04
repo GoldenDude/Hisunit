dfc5d49567a0d2b91dc9bf57e26995c1ba41bf93
GoldenDude <edanazran@gmail.com>
2018-06-04 17:15:05 +0300

    Fixed table.
    added automatic V or X inputs from the database.
    need to fix small V and X symbols

--- includes/javascript.js
+++ includes/javascript.js
@@ -1,43 +1,87 @@
  window.onload = function(){
     var json_data = [];
     var index = 0;
+    var notTreatedFound = 0;
+
+
 
     $.getJSON("data/MOCK_DATA.json", function (data) {
         console.log(data);
         json_data = data;
         for (var row of data) {
             
-
+            var checkTreated = row.treated.match("no");
+            
             if(index % 2){
-                var table_row = $(
-                    '<tr class = "marked">' +
-                        '<td>' + " <img src = 'images/X.png>' " + '</td>' +
+                if (checkTreated && notTreatedFound == 0){
+                    var table_row = $(
+                        '<tr class = "marked">' + 
+                        '<td>' + "<i class = 'fas fa-times-circle'></i> " + '</td>' +
+                        '<td class = "bold">' + row.treatment.t1 + ", " + row.treatment.t2 + '</td>' +
+                        '<td class = "bold">' + row.age + '</td>' +
+                        '<td class = "bold">' + row.name + '</td>' +
+                        '<td class = "bold">' + row.hour + '</td>' +
+                        '</tr>' 
+                        
+                    )
+
+                    notTreatedFound = 1; 
+                }
+
+                else{
+                    
+                    var table_row = $(
+                        '<tr class = "marked">' + 
+                        '<td>' + "<i class = 'fas fa-check-circle'></i> " + '</td>' +
                         '<td>' + row.treatment.t1 + ", " + row.treatment.t2 + '</td>' +
                         '<td>' + row.age + '</td>' +
                         '<td>' + row.name + '</td>' +
                         '<td>' + row.hour + '</td>' +
                         '</tr>' 
-                )
+                    )
+                }
+
+                
+                
                 ++index;
             }
 
             else{
-                var table_row = $(
+
+                if (checkTreated && notTreatedFound == 0){
+                    var table_row = $(
+                        '<tr>' + 
+                        '<td>' + "<i class = 'fas fa-times-circle'></i> " + '</td>' +
+                        '<td class = "bold">' + row.treatment.t1 + ", " + row.treatment.t2 + '</td>' +
+                        '<td class = "bold">' + row.age + '</td>' +
+                        '<td class = "bold">' + row.name + '</td>' +
+                        '<td class = "bold">' + row.hour + '</td>' +
+                        '</tr>' 
+                        
+                    )
+
+                    notTreatedFound = 1; 
+                }
+
+                else {
+                    var table_row = $(
                     '<tr>' +
-                        '<td>' + " " + '</td>' +
-                        '<td>' + row.treatment.t1 + ", " + row.treatment.t2 + '</td>' +
-                        '<td>' + row.age + '</td>' +
-                        '<td>' + row.name + '</td>' +
-                        '<td>' + row.hour + '</td>' +
+                    '<td>' + " <i class = 'fas fa-check-circle'></i> " + '</td>' +
+                    '<td>' + row.treatment.t1 + ", " + row.treatment.t2 + '</td>' +
+                    '<td>' + row.age + '</td>' +
+                    '<td>' + row.name + '</td>' +
+                    '<td>' + row.hour + '</td>' +
                     '</tr>'
-                )
+                    )
+                }
+
                 ++index;
             }
 
             var button = $('<button type="button" class = "pDetail">' + "פרטי טיפול" + '</button>');
 
-            table_row.prepend(button);
             $("tbody").append(table_row);
+            table_row.prepend(button);
 
         }
     })
